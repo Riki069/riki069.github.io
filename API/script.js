ecb2bf06d83a9cf9eda28bce67f241e700fa97cd
GigaChad00 <previak.richard.22@szlgbp.hu>
2025-02-19 20:21:30 +0100

    api 2.0 (+magyar forditás)

--- API/script.js
+++ API/script.js
@@ -1,5 +1,14 @@
 async function fetchQuote() {
-    const response = await fetch('https://api.kanye.rest');
-    const data = await response.json();
-    document.getElementById('quote').innerText = `"${data.quote}"`;
-  }
+  const response = await fetch("https://api.kanye.rest/");
+  const data = await response.json();
+  document.getElementById("quote").innerText = data.quote;
+}
+
+async function translateQuote() {
+  const quote = document.getElementById("quote").innerText;
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(quote)}&langpair=en|hu`;
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  document.getElementById("translated").innerText = data.responseData.translatedText;
+}
